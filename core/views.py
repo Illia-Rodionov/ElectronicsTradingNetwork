d6f055d2f2ca330dae0185dafeb812ae6872cecf
Illia-Rodionov <illia.rodionov@yandex.ru>
2023-02-16 12:48:40 +0300

    Creating views for models (User, Contact, Product, Supplier)

--- core/views.py
+++ core/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from django.db.models import Avg
+from rest_framework import viewsets, generics
+from core.models import Supplier, Product, User
+from core.permissions import IsActive
+from core.serializers import SupplierSerializer, ProductSerializer, \
+    SupplierStatisticSerializer, UserSerializer
 
-# Create your views here.
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class SupplierViewSet(viewsets.ModelViewSet):
+    queryset = Supplier.objects.all()
+    serializer_class = SupplierSerializer
+    permission_classes = (IsActive,)
+
+
+class SupplierStatisticView(generics.ListAPIView):
+    queryset = Supplier.objects.all()
+    serializer_class = SupplierStatisticSerializer
+    permission_classes = (IsActive,)
+
+    def get_queryset(self):
+        """View to get statistics about objects whose debt exceeds the average debt all objects"""
+        get_the_average = Supplier.objects.all().aggregate(Avg('debt_to_the_supplier'))
+        return Supplier.objects.filter(debt_to_the_supplier__gte=get_the_average['debt_to_the_supplier__avg'])
+
+
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    permission_classes = (IsActive,)
