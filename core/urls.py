7a4a916c52202b98ea2ea7c9c6f48a464f4b8a37
Illia-Rodionov <illia.rodionov@yandex.ru>
2023-02-16 13:02:30 +0300

    "Create a urls file and add urls for views."

---
+++ core/urls.py
@@ -0,0 +1,21 @@
+from django.urls import include, path
+from rest_framework import routers
+
+from core.views import SupplierViewSet, ProductViewSet, UserViewSet, ContactViewSet, SupplierStatisticView
+
+app_name = "core"
+
+
+router = routers.SimpleRouter()
+
+router.register(r"supplier", SupplierViewSet, basename='supplier')
+router.register(r"product", ProductViewSet, basename='product')
+router.register(r"user", UserViewSet, basename='user')
+router.register(r"contact", ContactViewSet, basename='contact')
+
+
+
+urlpatterns = [
+    path("", include(router.urls)),
+    path('debt', SupplierStatisticView.as_view(), name='debt'),
+]
