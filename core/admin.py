368491aac49a61f3c65c8abfbaeb69b89752f54a
Illia-Rodionov <illia.rodionov@yandex.ru>
2023-02-16 17:26:21 +0300

    a link to supplier has been added to the admin panel, and a filter by city

--- core/admin.py
+++ core/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
+from django.urls import reverse
+from django.utils.html import escape
+from django.utils.safestring import mark_safe
 
 from core.models import Product, User, Supplier, Contact
 
@@ -26,4 +29,19 @@ def clear_the_debt(modeladmin, request, queryset):
 
 @admin.register(Supplier)
 class SupplierAdmin(admin.ModelAdmin):
+    list_display = ['supplier_name', 'provider_link']
+    list_filter = ['contact__city']
     actions = [clear_the_debt]
+
+    def provider_link(self, obj):
+        """sending to the Supplier"""
+        if obj.provider:
+            link = reverse(f"admin:core_supplier_change",
+                           args=[obj.provider.pk])
+            return mark_safe(f'<a href="{link}">{escape(obj.provider.supplier_name)}</a>')
+        else:
+            return obj.provider
+
+    provider_link.allow_tags = True
+    provider_link.admin_order_field = 'provider'
+    provider_link.short_description = Supplier._meta.get_field('provider').verbose_name.title()
